8038ba98a2b6d979e4ae0d67788e6f50a3979b70
wengay <wengay@101.com>
2016-08-25 19:35:18 +0800

    线程池框架

--- src/main/java/com/nd/resource/transform/Application.java
+++ src/main/java/com/nd/resource/transform/Application.java
@@ -1,15 +1,23 @@
 package com.nd.resource.transform;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.concurrent.*;
 
 /**
  * Created by way on 2016/8/24.
  */
-@SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        ExecutorService executorService = MyExecutorService.newThreadPool();
+        try {
+            for(int i=0;i<50;i++){
+//                String output = executorService.submit(new MyTask(i)).get();
+                executorService.submit(new MyTask(i));
+            }
+        } catch (RejectedExecutionException e) {
+            e.printStackTrace();
+        }
+        executorService.shutdown();
     }
 }
